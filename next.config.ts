f0083e76c9a45b48ebabe94fdf70a5954de904d2
yundohyun <160732611+yundohyun@users.noreply.github.com>
2026-08-02 13:42:12 +0900

    feat: add `mdx` related packages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [["remark-toc", { heading: "The Table" }]],
+    rehypePlugins: [["rehype-katex", { strict: true, throwOnError: true }]],
+  },
+});
+
+export default withMDX(nextConfig);
